ce3185619c1568da716c38d84fdaf2af5fdf166d
Александра Матченя <matchenyasasha2@gmail.com>
2026-09-11 17:49:30 +0300

    Добавлен класс Protein для хранения данных белков

---
+++ laba01/src/Protein.cs
@@ -0,0 +1,16 @@
+﻿namespace GeneticSearch;
+
+public class Protein
+{
+    public string Name;
+    public string Organism;
+    public string AminoAcids;
+
+
+    public Protein(string name, string organism, string aminoAcids)
+    {
+        Name = name;
+        Organism = organism;
+        AminoAcids = aminoAcids;
+    }
+}
