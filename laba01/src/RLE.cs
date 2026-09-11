35c66b99783c84222a8d171fbd143a22901cc2a1
Александра Матченя <matchenyasasha2@gmail.com>
2026-09-11 17:51:18 +0300

    Добавлен класс RLE для кодирования и декодирования последовательностей

---
+++ laba01/src/RLE.cs
@@ -0,0 +1,62 @@
+﻿using System.Text;
+
+namespace GeneticSearch;
+
+public class RLE
+{
+
+    public string Decode(string text)
+    {
+        StringBuilder result = new();
+
+
+        for (int i = 0; i < text.Length; i++)
+        {
+            if (char.IsDigit(text[i]))
+            {
+                int count = text[i] - '0';
+                char letter = text[i + 1];
+
+                for (int j = 0; j < count; j++)
+                    result.Append(letter);
+
+                i++;
+            }
+            else
+                result.Append(text[i]);
+        }
+
+
+        return result.ToString();
+    }
+
+
+    public string Encode(string text)
+    {
+        StringBuilder result = new();
+
+        int count = 1;
+
+
+        for (int i = 0; i < text.Length; i++)
+        {
+            if (i + 1 < text.Length &&
+               text[i] == text[i + 1])
+            {
+                count++;
+            }
+            else
+            {
+                if (count > 2)
+                    result.Append(count);
+
+                result.Append(text[i]);
+
+                count = 1;
+            }
+        }
+
+
+        return result.ToString();
+    }
+}
